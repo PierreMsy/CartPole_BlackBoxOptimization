4f5f23b9a2c2c1c147360b519d1013582d5f09b3
Pierre Massey <pierremassey1@gmail.com>
2021-03-28 12:36:40 +0200

    feat : steepest ascent Hill Climbing implementation

--- policy.py
+++ policy.py
@@ -13,10 +13,20 @@ class Policy:
             np.random.rand(hiden_layer_size, action_size)
         ]
         self.best_w = self.w 
+        self.generations = {}
+
+    def create_generations(self, gen_size, scale):
+    
+        for i in range(gen_size):
+            self.generations[i] = self.create_noisy_network(scale)
+
         
     def add_noise(self, scale):
+        self.w = self.create_noisy_network(scale) 
 
-        self.w = [
+    def create_noisy_network(self, scale):
+
+        return [
             self.best_w[0] + np.random.normal(0, scale, self.best_w[0].size)\
                 .reshape(*self.best_w[0].shape),
             self.best_w[1] + np.random.normal(0, scale, self.best_w[1].size)\
@@ -28,6 +38,16 @@ class Policy:
         output = np.matmul(
             np.matmul(state, self.w[0]),
             self.w[1])
+        probas = softmax(output)
+
+        return np.random.choice([0,1], p=probas)
+
+    def act_with_generation(self, state, i_gen):
+        
+        w = self.generations[i_gen]
+        output = np.matmul(
+            np.matmul(state, w[0]),
+            w[1])
         probas = softmax(output)
 
         return np.random.choice([0,1], p=probas)
